6e2e68827ea90267058bbc02b59d7a52e7b5a5c5
super goon <binhminh3426@gmail.com>
2020-12-05 22:19:28 +0700

    mongoDB atlas - mongoose connect

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas quản lý database nhanh hơn với MongoDB Atlas
+
+const CONECTION_URL =
+    "mongodb+srv://fullstackmern:fullstackmern123@cluster0.rveu9.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
